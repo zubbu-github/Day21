db6d083048ca9aba83c6fcf35c09de3a0fded616
LENOVO <mdzubeir2468@gmail.com>
2022-03-07 21:38:55 +0530

    day 21 Actor Demo

---
+++ Day21/src/day21/Hero.java
@@ -0,0 +1,19 @@
+package day21;
+
+public class Hero implements Actor {
+	public void act() {
+		System.out.println("I can act");
+	}
+	public void speak() {
+		System.out.println("I can Speak");
+	}
+	public void comedy() {
+		System.out.println("I can do comedy");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Day21/src/day21/Villan.java
@@ -0,0 +1,18 @@
+package day21;
+
+public class Villan implements Actor{
+	public void act() {
+		System.out.println("I can act");
+	}
+	public void speak() {
+		System.out.println("I can Speak");
+	}
+	public void rude() {
+		System.out.println("I cant do comedy");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
